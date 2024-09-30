24a7eee387855d97fae8b65ac76d35b56ac15c9a
skibidi Greg <grgr.gllrn@gmail.com>
2024-09-30 14:14:11 +0200

    update utils

--- utils.py
+++ utils.py
@@ -1,26 +1,33 @@
-def ask_for_the_init_number() :
+ask_for_the_init_number_text = "Le nombre ? :"
 
-    pass
+ask_again_for_the_init_number_text = "Le nombre put*** ! : "
 
+bin_number_valid_chars = ["0", "1"]
 
-def ask_for_a_base(which_base) :
+dec_number_valid_chars = \
+    bin_number_valid_chars \
+  + ["2", "3", "4", "5", "6", "7", "8", "9"]
 
-    base = 0
+hex_number_valid_chars = \
+    dec_number_valid_chars \
+  + ["A", "B", "C", "D", "E", "F"] \
+  + ["a", "b", "c", "d", "e", "f"]
 
-    while base != 2 and base != 10 and base != 16 :
+def check_char_number_validity (char):
+    return char in hex_number_valid_chars
 
-        base = input(f"Quel est la base {which_base} ? 2 ; 10 ; 16 : ")
+def is_a_valid_number (number):
+    i = 0
+    is_a_valid_char = True
+    while is_a_valid_char == True and i <= len (number) - 1:
+        is_a_valid_char = check_char_number_validity (number [i])
+        i = i + 1
+    return is_a_valid_char
 
-    return base
-
-
-
-def ask_for_the_init_base() :
-
-    return ask_for_a_base("de début")
-
-
-
-def ask_for_the_target_base() :
-
-    return ask_for_a_base("visée")
+def ask_for_the_init_number ():
+    init_number = input (ask_for_the_init_number_text)
+    while not (is_a_valid_number (init_number)) == True:
+        init_number = input (ask_again_for_the_init_number_text)
+    return init_number
+        
+ask_for_the_init_number ()
