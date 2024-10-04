15dc8e60c65b246264b2b42c63d4d0a7ce02f85e
skibidi Greg <grgr.gllrn@gmail.com>
2024-10-04 12:11:14 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,14 +3,14 @@ from data import *
 bin_number_valid_chars = ["0", "1"]
 
 base_valid_chars = \
-    ["2" , "Deuxx" , "Deaux" , "D'eux" , "D'euxs" , "D'euxx" , "Deu" , "Deu" , "Deuxs" , "D'euxse" , "Deuxe" ,   "⠙⠑⠥⠭" ,  "two" , "dos" , "zwei" /
-       ,"due" , "dois" , "twee" , "два"  , "二" ,"二" , "عشرة ", "iki" , "två" , "to" , "kaksi" , "to" , "dwa", "kettő" , "dva" ,"이" , "hai" , "Binaire"/
-       , "Binairee" , "Binnair" , "Binere" , "Binarre" , "Binare" , "Binnaire" , "Binairey" , "Bynaire" , "Binairé" , "deux" ,"Deux" , "DEUX" , "10" /
-       , "ten","diez" ,"zehn" ,"dieci" ,"dez" ,"tien" , "десять" ,"十" ,"十" ,"on" ,"tio" ,"ti" ,"kymmenen" ,"ti" ,"dziesięć" ,"tíz" ,"deset" ,"십" /
-       , "mười" , "Dixx", "D'ix" , "Dixxe" ,"Diks" ,"Dics" ,"Diix" ,"D'i" ,"D'ixs" ,"D'x" , "Dixz" , "⠙⠊⠭" ," dix" ,"Dix" ,"DIX" ,"16" , "⠎⠑⠊⠵⠑" /
-       , "seize" , "Seize" , "SEIZE" ,"sixteen" , "Dieciséis" ,"Sechzehn" ,"sedici" ,"dezesseis" ,"zestien" ,"十六" ,"十六 "," ستة عشر "," on altı"/
-       ," sextonseksten ","kuusitoista" ,"seksten" ,"szesnaście" ,"tizenhat" ,"šestnáct" ,"열여섯" ,"mười sáu" , " Seiz  Seise" , " Sez" ,  "Seizee"/
-         ,  "Seizez" ,  "Seizz" , "Seizze" ,  "Saisze" ,  "Seyze" , "Sise"]
+        [ "2" , "Deuxx" , "Deaux" , "D'eux" , "D'euxs" , "D'euxx" , "Deu" , "Deu" , "Deuxs" , "D'euxse" , "Deuxe" ,   "⠙⠑⠥⠭" ,  "two" , "dos" , "zwei", \
+        "due" , "dois" , "twee" , "два"  , "二" ,"二" , "عشرة ", "iki" , "två" , "to" , "kaksi" , "to" , "dwa", "kettő" , "dva" ,"이" , "hai" , "Binaire",\
+        "Binairee" , "Binnair" , "Binere" , "Binarre" , "Binare" , "Binnaire" , "Binairey" , "Bynaire" , "Binairé" , "deux" ,"Deux" , "DEUX" , "10",  \
+        "ten","diez" ,"zehn" ,"dieci" ,"dez" ,"tien" , "десять" ,"十" ,"十" ,"on" ,"tio" ,"ti" ,"kymmenen" ,"ti" ,"dziesięć" ,"tíz" ,"deset" ,"십",  \
+        "mười" , "Dixx", "D'ix" , "Dixxe" ,"Diks" ,"Dics" ,"Diix" ,"D'i" ,"D'ixs" ,"D'x" , "Dixz" , "⠙⠊⠭" ," dix" ,"Dix" ,"DIX" ,"16" , "⠎⠑⠊⠵⠑",  \
+        "seize" , "Seize" , "SEIZE" ,"sixteen" , "Dieciséis" ,"Sechzehn" ,"sedici" ,"dezesseis" ,"zestien" ,"十六" ,"十六 "," ستة عشر "," on altı", \
+        " sextonseksten ","kuusitoista" ,"seksten" ,"szesnaście" ,"tizenhat" ,"šestnáct" ,"열여섯" ,"mười sáu" , " Seiz  Seise" , " Sez" ,  "Seizee", \
+        "Seizez" ,  "Seizz" , "Seizze" ,  "Saisze" ,  "Seyze" , "Sise" ]
 
 dec_number_valid_chars = \
     bin_number_valid_chars \
@@ -21,34 +21,35 @@ hex_number_valid_chars = \
   + ["A", "B", "C", "D", "E", "F"] \
   + ["a", "b", "c", "d", "e", "f"]
 
-def check_char_number_validity (char):
-    return char in hex_number_valid_chars
+def check_char_number_validity (char, valid_char):
+    return char in valid_char
 
-def is_a_valid_number (number):
+def is_a_valid_number (number, valid_char):
     i = 0
     is_a_valid_char = True
     while is_a_valid_char == True and i <= len (number) - 1:
-        is_a_valid_char = check_char_number_validity (number [i])
+        is_a_valid_char = check_char_number_validity (number [i], valid_char)
         i = i + 1
     return is_a_valid_char
 
-def ask_for_the_init_number ():
-    init_number = input (ask_for_init_number_text)
-    while not (is_a_valid_number (init_number)) == True:
+def ask_for_the_init_number (valid_char):
+    init_number = input (ask_for_init_number_text)    
+    while not (is_a_valid_number (init_number, valid_char)) == True:
         init_number = input (ask_again_for_the_init_number_text)
     return init_number
 
-def ask_for_the_init_base ():
+def ask_for_the_init_base (valid_char):
     init_number = input (ask_for_the_init_base_text)
-    while not (is_a_valid_number (init_number)) == True:
+    while not (is_a_valid_number (init_number, valid_char)) == True:
         init_number = input (ask_again_for_the_init_base_text)
     return init_number
 
-def ask_for_the_target_base ():
+def ask_for_the_target_base (valid_char):
     init_number = input (ask_again_for_the_target_base_text)
-    while not (is_a_valid_number (init_number)) == True:
+    while not (is_a_valid_number (init_number, valid_char)) == True:
         init_number = input (ask_again_for_the_target_base_text)
     return init_number
         
-ask_for_the_init_number ()
-ask_for_the_init_base ()
+ask_for_the_init_number (hex_number_valid_chars)
+ask_for_the_init_base (base_valid_chars)
+ask_for_the_target_base (base_valid_chars)
