351de8f899c5242c78b959f7d30af0fcd277f248
skibidi Greg <grgr.gllrn@gmail.com>
2024-10-04 11:28:00 +0200

    upadte utils

--- data.py
+++ data.py
@@ -1,2 +1,11 @@
-ask_for_init_number_text = "Le nombre ? :"
+ask_for_init_number_text = "Le nombre ? : "
+
 ask_again_for_the_init_number_text = "Le nombre put***! :"
+
+ask_for_the_init_base_text = "Quelle est la base du nombre ? : "
+
+ask_again_for_the_init_base_text = "Quelle est la base du nombre, choisissez un caractere valable ? : "
+
+ask_for_the_target_base_text = "Quelle est la base du nombre ? : "
+
+ask_again_for_the_target_base_text = "Quelle est la base ciblée stpp ?? : "
