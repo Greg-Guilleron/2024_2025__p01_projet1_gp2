f8d5333d36727528825ae3a0d36fda7de25f4483
skibidi Greg <grgr.gllrn@gmail.com>
2024-10-09 20:33:58 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -32,7 +32,7 @@ bin_base_valid_chars = [
     "second",    # En anglais (deuxième)
     "deuce",     # Variante anglaise informelle
     "bit",       # Terme lié à l'informatique
-    "binary2"    # Combinaison explicite
+    "binary2",   # Combinaison explicite  
 ]
 
 dec_base_valid_chars = [
@@ -78,7 +78,7 @@ hex_base_valid_chars = [
     "sixteenn",    # Variante stylisée
     "0x",          # Préfixe hexadécimal
     "hex16",       # Combinaison explicite
-    "16base"       # Variante numérique explicite
+    "16base",      # Variante numérique explicite
 ]
   
   
@@ -110,22 +110,69 @@ def is_a_valid_number (number, valid_char) :
     return is_a_valid_char
     
     
-def ask_for_the_init_number (valid_char) :
-    init_number = input (ask_for_init_number_text)    
+def ask_for_the_init_number (valid_char, question) :
+    init_number = input (question)    
     while not (is_a_valid_number (init_number, valid_char)) == True:
         init_number = input (ask_again_for_the_init_number_text)
     return init_number
 
-
-def ask_for_the_init_base (valid_char) :
-    init_base = input (ask_for_the_init_base_text)
+def ask_for_the_init_base (valid_char, question) :
+    init_base = input (question)
     while not (check_char_number_validity (init_base, valid_char)) == True:
         init_base = input (ask_again_for_the_init_base_text)
     return init_base
 
-
-def ask_for_the_target_base (valid_char) :
-    target_base = input (ask_for_the_target_base_text)
+def ask_for_the_target_base (valid_char, question) :
+    target_base = input (question)
     while not (check_char_number_validity (target_base, valid_char)) == True:
         target_base = input (ask_again_for_the_target_base_text)
     return target_base
+
+
+def sort_the_numbers_valid_chars_for_a_base(base) :
+    
+    match base :
+        case base if base in bin_base_valid_chars :
+            return bin_number_valid_chars
+        case base if base in dec_base_valid_chars :
+            return dec_number_valid_chars
+        case base if base in hex_base_valid_chars :
+            return hex_number_valid_chars
+        
+
+def bin_to_dec(init_number) :
+    target_number = 0
+    poids_binaire = len(init_number) - 1
+    for bit in init_number : 
+        target_number += int(bit) * 2**int(poids_binaire)
+        poids_binaire -= 1
+    return target_number
+
+def bin_to_hex(init_number) :
+    target_number = 0
+    pass
+    return target_number
+
+
+def dec_to_bin(init_number) :
+    target_number = 1
+    pass
+    return target_number
+
+
+def dec_to_hex(init_number) :
+    target_number = 2
+    pass
+    return target_number
+
+
+def hex_to_bin(init_number) :
+    target_number = 3
+    pass
+    return target_number
+    
+    
+def hex_to_dec(init_number) :
+    target_number = 4
+    pass
+    return target_number
